3b6584bdb52aaa29b518196103237fa4444d2b1e
Mohamed Shawkat <fagereleslamff@gmail.com>
2024-08-02 04:25:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// let a = 10;
+
+// let aw =
+//   a < 10
+//     ? console.log(10)
+//     : a >= 10 && a <= 40
+//     ? console.log("10 To 40")
+//     : a > 40
+//     ? console.log("> 40")
+//     : console.log("Unknown");
+
+// console.log(aw);
+
+// console.log("-------------------");
+
+// let st = "Elzero Web School"
+
+// if ("34" === "34") {
+//     console.log("Good")
+// }
+// if (typeof "st" === typeof "W") {
+//     console.log("Good")
+// }
+
+// if () {
+
+// }
+
+// 4:01:00
+
+
+
+// -------------------------------------
+
+let myFriends = ["Mohamed" , "Shawkat" , "Agwa"];
+
+console.log(`Hellow ${myFriends[0][3]}`)
